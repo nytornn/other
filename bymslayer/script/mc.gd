e6b485e3bfa2ca2b3c55372ba83a9d30558a331c
nytornn <76937551+nytornn@users.noreply.github.com>
2024-03-26 23:36:26 +0100

    Update mc.gd

--- bymslayer/script/mc.gd
+++ bymslayer/script/mc.gd
@@ -21,7 +21,6 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
